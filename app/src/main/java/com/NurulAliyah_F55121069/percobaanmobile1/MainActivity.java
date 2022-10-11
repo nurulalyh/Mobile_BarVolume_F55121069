fcca739509c4f04991e25d2c8e7b0e2424e57ca8
nurulalyh <nurulaliyah2303@gmail.com>
2022-10-12 05:00:34 +0800

    first commit

---
+++ app/src/main/java/com/NurulAliyah_F55121069/percobaanmobile1/MainActivity.java
@@ -0,0 +1,76 @@
+package com.NurulAliyah_F55121069.percobaanmobile1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText edtWidth;
+    private EditText edtHeight;
+    private EditText edtLength;
+    private TextView tvResult;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtWidth = findViewById(R.id.edt_width);
+        edtHeight = findViewById(R.id.edt_height);
+        edtLength = findViewById(R.id.edt_length);
+        Button btnCalculate = findViewById(R.id.btn_calculate);
+        tvResult = findViewById(R.id.tv_result);
+        btnCalculate.setOnClickListener(this);
+
+        if (savedInstanceState != null) {
+            String result = savedInstanceState.getString(STATE_RESULT);
+            tvResult.setText(result);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_calculate) {
+            String inputLength = edtLength.getText().toString().trim();
+            String inputWidth = edtWidth.getText().toString().trim();
+            String inputHeight = edtHeight.getText().toString().trim();
+
+            boolean isEmptyFields = false;
+
+            if (TextUtils.isEmpty(inputLength)) {
+                isEmptyFields = true;
+                edtLength.setError("Field ini tidak boleh kosong");
+            }
+
+            if (TextUtils.isEmpty(inputWidth)) {
+                isEmptyFields = true;
+                edtWidth.setError("Field ini tidak boleh kosong");
+            }
+
+            if (TextUtils.isEmpty(inputHeight)) {
+                isEmptyFields = true;
+                edtHeight.setError("Field ini tidak boleh kosong");
+            }
+
+            if (!isEmptyFields) {
+                Double volume = Double.parseDouble(inputLength) * Double.parseDouble(inputWidth) * Double.parseDouble(inputHeight);
+                tvResult.setText(String.valueOf(volume));
+            }
+        }
+    }
+
+    private static final String STATE_RESULT = "state_result";
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+            super.onSaveInstanceState (outState);
+            outState.putString(STATE_RESULT, tvResult.getText().toString());
+    }
+}
